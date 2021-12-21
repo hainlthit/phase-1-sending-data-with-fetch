384d96ba5b56ae4a6aef6f9bc7c7f5d875f68dc2
Hain Thit <hainlthit@gmail.com>
2021-12-20 23:16:34 -0500

    commit tree d67763a69097332c6d697c501ea57e0431b0d949

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const newData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(newData),
+}
+
+function submitData() {
+return fetch("http://localhost:3000/users", configData)
+.then(resp => resp.json())
+.then(getID)
+.catch(errorFunc)
+}
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+function errorFunc(error) {
+    document.body.innerHTML = error
+}
